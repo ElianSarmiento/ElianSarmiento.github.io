36fb56875eb28b8718807e69446aba545d009b22
Elian <43243730+ElianSarmiento@users.noreply.github.com>
2025-03-28 19:15:30 -0400

    Delete Spring-2025-BootCamp-work directory

--- Spring-2025-BootCamp-work/Day 2/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("popup").addEventListener("click", function () {
-    alert("This is your pop-up message!");
-});
-
-
-document.querySelector(".popup-button").addEventListener("click", function () {
-    alert("You clicked the button!");
-});
-
