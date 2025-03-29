395c1746356aa4ba582d970321f201a8a1f4ad83
Elian <43243730+ElianSarmiento@users.noreply.github.com>
2025-03-29 17:22:36 -0400

    Add files via upload

---
+++ Spring-2025-BootCamp-work/HW2/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".popup-button").addEventListener("click", function () {
+    alert("This is your pop-up message!");
+});
+document.querySelector(".home-tab").addEventListener("click", function () {
+    alert("Welcome home!");
+});
